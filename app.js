c3952d7932a19f6a216bdf544eef5ef040983964
nicolausmaximus <aniket261234@gmail.com>
2022-01-06 23:34:07 +0530

    created  dbconnect.js to improve readability

--- app.js
+++ app.js
@@ -1,25 +1,11 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const fs = require("fs");
-var mysql = require('mysql');
+var db = require('./dbconnect');
 var https = require('follow-redirects').https;
 require('events').EventEmitter.prototype._maxListeners = 5;
 
-var db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'airtribe',
-    multipleStatements: true
-});
 
-db.connect(function(err) {
-    if (err) throw err;
-    let createtable = 'create table if not exists stackquestion (Number int(10) NOT NULL, Question text NOT NULL, Description text NOT NULL, Views varchar(1000) NOT NULL, UpVotes varchar(1000) NOT NULL, Answers varchar(1000) NOT NULL)';
-    db.query(createtable, function(err, result, fields) {
-        if (err) throw err;
-    });
-});
 
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const csvWriter = createCsvWriter({
